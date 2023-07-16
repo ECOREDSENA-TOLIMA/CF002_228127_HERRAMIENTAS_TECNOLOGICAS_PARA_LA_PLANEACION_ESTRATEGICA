e9c8b78894668fef38a7f84659f0d2c263b8a10e
Oscar Espitia <odespitia@sena.edu.co>
2023-07-16 12:33:52 -0500

    tema 2

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -7,6 +7,140 @@
         span 2
       h1 Técnicas de priorización
 
+    p.mb-3(data-aos="flip-left") La disponibilidad de recursos es uno de los factores de mayor complejidad a la hora de administrar una organización. Todo trabajo a llevar a cabo debe ser necesario para la generación de valor, que es el fin último de cualquier empresa; la lógica del costo de oportunidad, aplicable a prácticamente cualquier tarea sea o no del ámbito organizacional, indica que cada vez que se elige una alternativa entre varias se está renunciando a los beneficios de las demás. 
+    p.mb-5(data-aos="flip-left") En muchas ocasiones, las empresas no cuentan con los recursos necesarios para implementar todas las estrategias que aporten a su crecimiento, por lo que se hace necesario tomar decisiones. Definir cuál es la opción más viable, bien sea porque es más provechosa, más fácil o rápida de implementar o más rentable, es una tarea que inevitablemente deja rezagadas a otras.
+
+    .row.align-items-center.justify-content-center.mb-4(data-aos="fade-down-left")
+      .col-12.col-md-10.col-lg-8
+        .cajon.color-acento-contenido.p-3
+          p.mb-0 La priorización es una metodología centrada en la presentación de tareas siguiendo un orden determinado, en la que a cada una de ellas se le otorga un nivel de importancia. De esta forma, el trabajo se puede realizar siguiendo la secuencia diseñada en función de estos criterios. 
+
+    p.mb-3(data-aos="flip-left") Priorizar resulta necesario e ineludible, pues las empresas no pueden contar con la totalidad de los recursos de capital, tiempo o personal para llevar a cabo todas las tareas de forma simultánea. Si existen 5 estrategias comerciales aplicables para mejorar los niveles de ventas, probablemente no habrá dinero suficiente para pagar todas las campañas publicitarias, ni vendedores o jefes de área que supervisen la evolución y el impacto; tampoco podría tenerse certeza de cuál de las estrategias es la más efectiva y está dando los mejores resultados.
+    p.mb-3(data-aos="flip-left") Por lo anterior, siempre la implementación de actividades se hace de forma lineal y no paralela, porque primero se debe corroborar la efectividad de una, antes de implementar la siguiente. Una vez se tiene certeza de lo apropiado de una medida, puede resultar conveniente probar otros métodos y, a través de un análisis comparativo, elegir aquella que sea más efectiva o proponer estrategias que las combinen para maximizar los resultados.
+    p.mb-4(data-aos="flip-left") En la actualidad, existen diversas herramientas que funcionan como apoyo para la toma de esta clase de decisiones, al fin y al cabo, aunque parezca un proceso intuitivo, estructurarlo con parámetros concretos puede dotarlo de mayor objetividad y minimizar la probabilidad de error. Los siguientes son algunos de los sistemas utilizados en la práctica:
+
+    .h4.py-4.mb-4(data-aos="zoom-down-right")
+      span.titulo-herramientas Matriz de priorización
+
+    p.mb-5(data-aos="flip-left") Esta herramienta corresponde a una tabla en la que se listan las opciones y cada uno de los criterios que apoyan o controvierten la elección de cada una de ellas. Funcionan de forma muy sencilla; pero, pueden aportar en la toma de decisiones. El objetivo es que al final se elija la opción que según los criterios dispuestos alcance mayor puntuación y se considere la más adecuada. Para utilizar este instrumento es importante tener claridad de los elementos clave y la forma en la que se debe proceder. Vea los pasos más relevantes: 
+
+    .tarjeta.color-primario.p-4.mb-5
+      LineaTiempoC.color-acento-contenido(text-small)
+        .row.align-items-center.justify-content-center(titulo="1")
+          .col-md-11.col-12.mb-4.mb-md-0
+            h4 Definir el objetivo
+            p En primer lugar, debe tenerse claridad sobre lo que se busca potenciar, mitigar o solucionar. Las opciones para abordar esta situación solo pueden evaluarse, si primero se tiene claro lo que se espera alcanzar con su implementación. 
+        .row.align-items-center.justify-content-center(titulo="2")
+          .col-md-11.col-12.mb-4.mb-md-0
+            h4 Listar las alternativas
+            p Una vez se tiene claro qué se espera abordar se deben presentar las opciones disponibles; es aconsejable que el listado no sea tan extenso, puesto que puede dificultar el análisis posterior. Si el trabajo es en equipo, puede hacerse un filtro preliminar para que la matriz tenga únicamente las opciones más viables.
+        .row.align-items-center.justify-content-center(titulo="3")
+          .col-md-11.col-12.mb-4.mb-md-0
+            h4 Determinar los criterios
+            p Estos son los elementos de juicio que posteriormente sustentarán la elección de una opción, dependiendo del objetivo definido previamente, variarán los criterios a disponer, por ejemplo, si es una medida centrada en la generación de utilidades, algunos de los criterios más relevantes serán el costo de implementación, la certeza de la generación de utilidades y el volumen esperado de estas.
+        .row.align-items-center.justify-content-center(titulo="4")
+          .col-md-11.col-12.mb-4.mb-md-0
+            h4 Asignación de valor
+            p En este paso se deben ponderar los criterios, lo que implica darles un nivel de importancia según la escala que defina el equipo. Qué tanto peso tiene un criterio sobre el consenso general, es un aspecto clave, pues implica que algunas alternativas en principio similares a otras puedan ser elegidas, debido a que resaltan en los criterios más relevantes. 
+        .row.align-items-center.justify-content-center(titulo="5")
+          .col-md-11.col-12.mb-4.mb-md-0
+            h4 Comparación de las alternativas
+            p Para este punto se debe hacer un análisis de cada opción y puntuarlas en función de la ponderación previa; de esta manera se “califica” cada criterio otorgándole un valor, que puede ser comparado con los de otras opciones listadas.
+        .row.align-items-center.justify-content-center(titulo="6")
+          .col-md-11.col-12.mb-4.mb-md-0
+            h4 Elección de la opción
+            p Una vez se han cuantificado todos los criterios, solo resta hacer la suma de todas las valoraciones obtenidas por cada alternativa y elegir aquella con el puntaje conjunto más elevado. De esta forma se conserva la objetividad, al dejar la elección condicionada a una operación aritmética. 
+
+    p.mb-4(data-aos="flip-left") Se presenta el siguiente ejemplo para comprender su aplicabilidad: 
+    figure.mb-5(data-aos="zoom-down-right")
+      .video
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/npTMvq02U0w" title="Ejemplo matriz de priorización" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+    
+    p.mb-4(data-aos="flip-left") Existen otros sistemas de priorización utilizados; pero, su eje fundamental opera bajo el mismo sistema, es decir, determinar qué es lo más importante a través de una serie de criterios que contemplan tiempo, recursos, efectividad y esfuerzo. Vea esta breve reseña de algunos de ellos:
+
+    AcordionA.mb-5(tipo="a" clase-tarjeta="tarjeta color-primario" data-aos="flip-left")
+      .row(titulo="Pirámide de priorización")
+        .col-md-11.col-12.mb-4.mb-md-0
+          p Una pirámide es una estructura con una base ancha, con mayor capacidad para contener actividades y soporta los niveles medio y superior, por lo que la priorización funciona en este sentido. En la cúspide se ubican las tareas o actividades más importantes que requieren especial atención; en el nivel intermedio aquellas que no se deben realizar tan pronto, pero que igual tienen importancia. En la base se ubican las tareas que no son imprescindibles y que, por lo tanto, si la situación durante el desarrollo de las demás fases lleva a ello, pueden ser dejadas a un lado en busca de la mayor efectividad con el menor costo. Cada nivel debe tener menos actividades que su inferior, de la misma forma, la prioridad será más alta según más arriba se encuentre ubicada.
+      .row(titulo="Método RICE")
+        .col-md-11.col-12.mb-4.mb-md-0
+          p Por sus siglas en inglés que traducen alcance (R), impacto (I), confianza (C) y esfuerzo (E). Es un sistema que busca calcular a través de una fórmula sencilla el valor de un índice que determinará cuál actividad realizar. La fórmula en concreto es la siguiente:
+          p R*I*C/E 
+          p De la fórmula se desprende que se favorecerán las opciones con mayor alcance, impacto y confianza al menor esfuerzo. Así, a todas las propuestas se les deberá asignar un valor por cada elemento y efectuar el cálculo para determinar cuál tiene el índice más alto.
+      .row(titulo="Principio de Pareto")
+        .col-md-11.col-12.mb-4.mb-md-0
+          p Una máxima bastante popular en cualquier ámbito, el principio de Pareto indica que el 80% de los resultados provienen de un 20% del esfuerzo. Esto apunta a que en cualquier aspecto de la vida siempre habrá una proporción (cercana al 20%) de situaciones que motivan o desencadenan el 80% de las consecuencias. Para hacer uso de esta metodología, basta con listar las actividades o alternativas a desarrollar para una problemática concreta, se asigna un valor porcentual en función de algún criterio de medición (frecuencia, impacto, facilidad, beneficio asociado) y se lista de mayor a menor porcentaje. La suma total debe ser equivalente al 100% y debe prestarse especial atención a aquellas situaciones que en conjunto representan la mayor proporción de tal porcentaje. 
+
+    p.mb-4(data-aos="flip-left") Observe el siguiente ejemplo para comprender mejor el principio de Pareto:
+
+    .row.align-items-center.justify-content-center.mb-4(data-aos="fade-down-left")
+      .col-12.col-md-11.p-4(style="background-color:#FFF7C4")
+        p Una empresa recibe quejas respecto a los productos que vende, actualmente cuenta con 2.000 solicitudes de devolución por los siguientes motivos:
+        ul.lista-ul.mb-2
+          li.mb-0
+            i.fas.fa-angle-right
+            | Fallas en los productos: 300
+          li.mb-0
+            i.fas.fa-angle-right
+            | Diferencias con lo mostrado en anuncios: 600
+          li.mb-0
+            i.fas.fa-angle-right
+            | Mala atención posventa: 800
+          li.mb-0
+            i.fas.fa-angle-right
+            | Puede estar acompañador de recuadro color de fondo
+          li.mb-0
+            i.fas.fa-angle-right
+            | Deterioro acelerado durante los primeros meses de uso: 300
+        
+        p Se listan las causales de mayor a menor y se asigna el porcentaje de participación a cada uno de ellos, de igual forma, se presenta una columna en la que se vayan acumulando los porcentajes:
+            
+        .tabla-a.color-primario.mb-4
+          table
+            thead
+              tr
+                th Situación
+                th Frecuencia
+                th Participación
+                th Acumulado
+            tbody
+              tr 
+                td.text-center Mala atención posventa
+                td.text-center 800
+                td.text-center 40%
+                td.text-center 40%
+              tr 
+                td.text-center Diferencias con anuncios
+                td.text-center 600
+                td.text-center 30%
+                td.text-center 70%
+              tr 
+                td.text-center Fallas en los productos
+                td.text-center 300
+                td.text-center 15%
+                td.text-center 85%
+              tr 
+                td.text-center Deterioro acelerado
+                td.text-center 300
+                td.text-center 15%
+                td.text-center 100%
+              tr 
+                th.text-center Total
+                th.text-center 2.000
+                th.text-center 100%
+                th.text-center
+
+        p(data-aos="fade-left") De la tabla anterior se interpreta que la mala atención después de la venta y la publicidad engañosa son las dos problemáticas que deben ser abordadas con mayor prioridad, puesto que concentran el 70% de los reclamos efectuados por los clientes. 
+
+
+
+
+
+
+
+
+
+
 </template>
 
 <script>
